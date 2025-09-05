670ded5c015caf2a91da8d4c07723569e163dec4
opilane <opilane@HP-CZC2349HTZ>
2025-09-05 11:40:24 +0300

    Add project files.

---
+++ osa_2C/funk.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osa_2C
+{
+    internal class funk
+    {
+            public static string pikkus(int pikknr)
+            {
+                string pikk = "";
+                if (pikknr <= 130)
+                {
+                    pikk = "luhike";
+                }
+                else if (pikknr >= 140 && pikknr <= 150)
+                {
+                    pikk = "kesk";
+                }
+                else if (pikknr >= 160 && pikknr <= 180)
+                {
+                    pikk = "pikk";
+                }
+                else if (pikknr >= 190)
+                {
+                    pikk = "vaga pikk";
+                }
+                else
+                {
+                    pikk = "???";
+                }
+                return pikk;
+
+            }
+        }
+}
---
+++ osa_2C/juku.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace osa_2C
+{
+    internal class Juku
+    {
+        //public static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Mis sinu nimi on?");
+        //    string eesnimi = Console.ReadLine();
+
+        //    if (eesnimi.ToLower() == "juku")
+        //    {
+        //        Console.WriteLine("Lähme kinno");
+        //        Console.WriteLine("Sisesta vanus:");
+        //        string vanusSisend = Console.ReadLine();
+
+
+        //        int vanusjuku = int.Parse(vanusSisend);
+
+        //        string pilet = vanus(vanusjuku);
+        //        Console.WriteLine("Sinu pilet on: " + pilet);
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Me ei lähe kinno");
+        //    }
+        //}
+
+        //public static string vanus(int vanusjuku)
+        //{
+        //    string pilet = "";
+
+        //    if (vanusjuku <= 0 || vanusjuku > 100)
+        //    {
+        //        pilet = "viga";
+        //    }
+        //    else if (vanusjuku <= 6)
+        //    {
+        //        pilet = "tasuta";
+        //    }
+        //    else if (vanusjuku <= 14)
+        //    {
+        //        pilet = "lastepilet";
+        //    }
+        //    else if (vanusjuku <= 65)
+        //    {
+        //        pilet = "täispilet";
+        //    }
+        //    else
+        //    {
+        //        pilet = "sooduspilet";
+        //    }
+
+        //    return pilet;
+        //}
+        public static void temp(string[] args)
+        {
+            Console.WriteLine("Milline temperatuur on toas?");
+            double temp = double.Parse(Console.ReadLine());
+            if (temp >= 18)
+            {
+                Console.WriteLine("toas on soe");
+            }
+
+            else
+            {
+                Console.WriteLine("toas on kulm");
+            }
+        }
+            public static void Main(string[] args)
+        {
+            Console.WriteLine("kas sa tahad tead pikk?");
+            string vas = Console.ReadLine();
+            if (vas.ToLower() != "jah")
+            {
+                Console.WriteLine("bebe loh ");
+            }
+            else
+            {
+                try
+                {
+                    Console.Write("kirjuta oma pikkust:");
+                    int pikk = int.Parse(Console.ReadLine());
+                    string pikk1 = funk.pikkus(pikk);
+                    Console.WriteLine($"sina oled: {pikk1}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+
+            }
+
+
+
+
+        }
+
+    }
+}
